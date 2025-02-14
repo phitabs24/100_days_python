3a99398308494a2bbbf526c2ff756e1182f12141
Achu Philemon Tabufor <phitabs24@gmail.com>
2025-02-14 04:30:51 -0800

    spirograph done

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,45 +1,53 @@
 import random
-from turtle import Turtle, Screen
+
+from turtle import Turtle, Screen, colormode
 
 t = Turtle()
+colormode(255)
 # t.pu()
 # t.setpos(-100, 350)
 # t.pd()
-n = 1000
+# n = 1000
 
-color_list = ["CadetBlue",
-"CadetBlue1",
-"CadetBlue2",
-"CadetBlue3",
-"CadetBlue4",
-"chartreuse",
-"chartreuse1",
-"chartreuse2",
-"chartreuse3",
-"chartreuse4",
-"chocolate",
-"chocolate1",
-"chocolate2", "NavajoWhite4",
-"navy",
-"NavyBlue",
-"OldLace",
-"OliveDrab",
-"OliveDrab1",
-"OliveDrab2",
-"OliveDrab3",
-"OliveDrab4",
-"orange",
-"orange1",
-"orange2",
-"orange3",
-"orange4",
-"OrangeRed",
-"OrangeRed1",
-"OrangeRed2",
-"OrangeRed3",
-"OrangeRed4",
-"orchid"]
+# color_list = ["CadetBlue",
+# "CadetBlue1",
+# "CadetBlue2",
+# "CadetBlue3",
+# "CadetBlue4",
+# "chartreuse",
+# "chartreuse1",
+# "chartreuse2",
+# "chartreuse3",
+# "chartreuse4",
+# "chocolate",
+# "chocolate1",
+# "chocolate2", "NavajoWhite4",
+# "navy",
+# "NavyBlue",
+# "OldLace",
+# "OliveDrab",
+# "OliveDrab1",
+# "OliveDrab2",
+# "OliveDrab3",
+# "OliveDrab4",
+# "orange",
+# "orange1",
+# "orange2",
+# "orange3",
+# "orange4",
+# "OrangeRed",
+# "OrangeRed1",
+# "OrangeRed2",
+# "OrangeRed3",
+# "OrangeRed4",
+# "orchid"]
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 # while n < len(color_list):
 #     color = random.choice(color_list)
 #     t.color(color)
@@ -49,29 +57,22 @@ color_list = ["CadetBlue",
 #         t.rt(angle)
 #     n += 1
 
-def west():
-    t.setheading(180)
-
-def east():
-    t.setheading(0)
-
-def north():
-    t.setheading(90)
-
-def south():
-    t.setheading(270)
 
 directions = [0, 90, 270, 180]
 t.speed(10)
-t.pensize(20)
+# t.pensize(20)
 
-for _ in range(n):
-    t.pencolor(random.choice(color_list))
-    t.setheading(random.choice(directions))
-    t.forward(25)
+# for _ in range(n):
+#     t.pencolor(random_color())
+#     t.setheading(random.choice(directions))
+#     t.forward(30)
 
-
-
+n = 0
+for _ in range(36):
+    t.pencolor(random_color())
+    t.setheading(n)
+    t.circle(120)
+    n += 10
 
 
 
