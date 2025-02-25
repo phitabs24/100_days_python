54c74e8d802b7f0c7f9543ef6e232b3d2bf8dc09
Achu Philemon Tabufor <phitabs24@gmail.com>
2025-02-25 14:20:46 -0800

    starting day 18 pomodora app project

--- Day-28-Pomodora/main.py
+++ Day-28-Pomodora/main.py
@@ -1,3 +1,5 @@
+import math
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,11 +10,79 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+checks = ""
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    reps = 0
+    cycles.config(text="")
+    title.config(text="Timer", fg=GREEN)
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    global reps
+    reps += 1
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    work_sec = WORK_MIN * 60
+    short_br_sec = SHORT_BREAK_MIN * 60
+    long_br_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        title.config(text="Break", fg=RED)
+        count_down(long_br_sec)
+    elif reps % 2 == 0:
+        title.config(text="Break", fg=PINK)
+        count_down(short_br_sec)
+    else:
+        title.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global checks
+    global timer
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}"
+    if count > 0:
+        canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        if reps % 2 == 0:
+            checks += "✔"
+            cycles.config(text=checks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW, background=YELLOW)
+tomato_img = PhotoImage(file="tomato.png")
+
+canvas = Canvas(width=200, height=224, background=YELLOW, highlightthickness=0)
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 138, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+title = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+title.grid(column=1, row=0)
+
+start = Button(text="Start", font=(FONT_NAME, 14, "normal"), command=start_timer)
+start.grid(column=0, row=2)
+
+reset = Button(text="Reset", font=(FONT_NAME, 14, "normal"), command=reset_timer)
+reset.grid(column=2, row=2)
+
+cycles = Label(font=(FONT_NAME, 16, "bold"), fg=GREEN, bg=YELLOW)
+cycles.grid(column=1, row=3)
+
+window.mainloop()
